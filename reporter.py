980ea4dc5836862566e9b56a882b72ce0c1dccb6
Raj Nath Sah <raj4u4all@gmail.com>
2019-08-15 21:43:35 +0400

    updated reporter.py file

--- reporter.py
+++ reporter.py
@@ -1,7 +1,13 @@
 import papermill as pm
+import os
 
-pm.execute_notebook(
-   'template.ipynb',
-   'sales_feb.ipynb',
-   parameters=dict(filename="sales_feb.xlsx")
-)
+# Function to generate report as jupypter notebook and save graph as image file in same directory
+def gen_report(template,out_notebook,datafile):
+    pm.execute_notebook(template,out_notebook,parameters=dict(filename=datafile))
+
+# Read all data files present in folder "monthly_data" and generate report for all one by one    
+try:    
+    for filename in os.listdir("monthly_data"):        
+        gen_report('template.ipynb',filename.replace('.xlsx','.ipynb'),os.path.abspath('monthly_data\{}'.format(filename)))
+except Exception as ex:
+    print('Error in generating report, Please check data file and check if report is already generated.')
